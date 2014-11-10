163ea2b5ced6ac47857bb03bf4ff67742095c742
Jim Barritt <jim@planet-ix.com>
2014-11-10 21:47:54 +0000

    [jmdb] Trying to ssh to the right place

--- ops/deploy.py
+++ ops/deploy.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 import os, sys, time, datetime
+from subprocess import call
 
-print "Going to deploy to [" + sys.argv[1] + "] ..."
+environment = sys.argv[1]
+ssh_login = os.environ["IXCODE_SSH_" + environment]
+
+print "Going to deploy to [" + environment + "] ..."
 
 print "Using key [" + os.environ['IXCODE_ENV_KEY'] + "] ..."
+
+call(["ssh", "-v", ssh_login, "ls"])
 
 ts = time.time()
 
--- ops/install-build-tools.py
+++ ops/install-build-tools.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python
 
-import os.path
 from subprocess import call
 
 
-def ensure_dir(f):
-    d = os.path.dirname(f)
-    if not os.path.exists(d):
-        os.makedirs(d)
-
-
-# ensure_dir("target")
-
-
-call(["cd", "target"])
-call(["git"])
+call(["pwd"])
